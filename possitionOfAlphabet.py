dd77c165130ef77fdbcdae57681573be2ffdc123
hm-chandan <36477685+hm-chandan@users.noreply.github.com>
2018-02-15 18:17:48 +0530

    Update possitionOfAlphabet.py

--- possitionOfAlphabet.py
+++ possitionOfAlphabet.py
@@ -1,3 +1,8 @@
+#enter a alphabet as input
 b = input(str())
+#if the given alphabet is capital letter convert it to lower case letter
 v= b.lower()
+#display the given alphbet position
 print(ord(v) - 96)
+
+#sample input D output will be 4 or if input is z output will be 26
