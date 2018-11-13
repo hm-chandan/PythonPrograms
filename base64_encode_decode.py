73a5de59abc493220f398705ca4e77a6481e9a65
chandan <36477685+hm-chandan@users.noreply.github.com>
2018-11-13 12:44:34 +0530

    Update base64_encode_decode.py

--- base64_encode_decode.py
+++ base64_encode_decode.py
@@ -1,11 +1,11 @@
 import base64
 
-
+'''
 encoded = base64.b64encode( bytes("sachin", "utf-8") )
 print(encoded)
 decoded = base64.b64decode(encoded)
 print(decoded)
-
+'''
 
 
 encoded = base64.b64encode(b"sachin")
